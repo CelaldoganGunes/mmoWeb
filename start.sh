1e891d588ef4f89b16b67d42fef71d84d3a667e0
Celaldogan Gunes <celaldogan2002@outlook.com>
2023-12-02 01:13:17 +0300

    parameter to identify server on htop

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 mkdir logs
 CURRENTDATE=`date +"%Y-%m-%d %H-%M-%S %z"`
-node index.js 2>&1 | tee -a "./logs/${CURRENTDATE}.txt" #DEFAULT SYSTEM
+node index.js -webServer 2>&1 | tee -a "./logs/${CURRENTDATE}.txt" #DEFAULT SYSTEM
 #pm2 start index.js --name webServer --output "./logs/${CURRENTDATE}.txt" --error "./logs/${CURRENTDATE}.txt"
 # Check CRLF LF setting
